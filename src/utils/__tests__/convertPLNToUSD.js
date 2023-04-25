902ddb340c6b239f88bb57a2ede82b8125c5d21a
Slawek Derabin <slawomirderabin@gmail.com>
2023-04-25 18:41:08 +0200

    fix number to string

--- src/utils/__tests__/convertPLNToUSD.js
+++ src/utils/__tests__/convertPLNToUSD.js
@@ -1,17 +1,23 @@
-import { convertPLNToUSD } from './../convertPLNtoUSD';
+import { USD_TO_PLN, convertPLNToUSD } from './../convertPLNtoUSD';
+
+
+const convertedToDolar = (pln) => `$${((pln / USD_TO_PLN).toFixed(2))}`
+
 
 describe('ConvertPLNtoUSD', () => {
   it('should return proper value when good input', () => {
-    expect(convertPLNToUSD(1)).toBe('$0.29');
-    expect(convertPLNToUSD(2)).toBe('$0.57');
-    expect(convertPLNToUSD(20)).toBe('$5.71');
-    expect(convertPLNToUSD(12)).toBe('$3.43');
+    expect(convertPLNToUSD(1)).toBe(convertedToDolar(1));
+    expect(convertPLNToUSD(2)).toBe(convertedToDolar(2));
+    expect(convertPLNToUSD(20)).toBe(convertedToDolar(20));
+    expect(convertPLNToUSD(12)).toBe(convertedToDolar(12));
+    expect(convertPLNToUSD('6')).toBe(convertedToDolar(6));
+    expect(convertPLNToUSD('16')).toBe(convertedToDolar(16));
+    expect(convertPLNToUSD('26')).toBe(convertedToDolar(26));
+
   });
 
   it('should return NaN when input is text', () => {
-    expect(convertPLNToUSD('6')).toBeNaN();
     expect(convertPLNToUSD('abc')).toBeNaN();
-    expect(convertPLNToUSD('-543')).toBeNaN();
   });
 
   it('should return NaN when input is empty', () => {
@@ -22,13 +28,14 @@ describe('ConvertPLNtoUSD', () => {
     expect(convertPLNToUSD({})).toBe('Error');
     expect(convertPLNToUSD([])).toBe('Error');
     expect(convertPLNToUSD(null)).toBe('Error');
-    expect(convertPLNToUSD(function() {})).toBe('Error');
+    expect(convertPLNToUSD(function () { })).toBe('Error');
   });
-  
+
   it('should return zero when input is lower than zero', () => {
-    expect(convertPLNToUSD(-1)).toBe('$0.00');
-    expect(convertPLNToUSD(-2)).toBe('$0.00');
-    expect(convertPLNToUSD(-56)).toBe('$0.00');
+    expect(convertPLNToUSD(-1)).toBe(convertedToDolar(0));
+    expect(convertPLNToUSD(-2)).toBe(convertedToDolar(0));
+    expect(convertPLNToUSD(-56)).toBe(convertedToDolar(0));
+    expect(convertPLNToUSD('-543')).toBe(convertedToDolar(0));
   });
 
 });
--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,31 +1,31 @@
+
+export const USD_TO_PLN = 5.5;
+
 export const convertPLNToUSD = (PLN) => {
-  if (typeof PLN === 'string') {
-    return NaN;
-  }
-  
-  if ( typeof PLN === 'undefined' ) {
-    return NaN;
-  }
-
-  if ( typeof PLN === 'object') {
-    return 'Error';
-  }
-
-  if ( typeof PLN === 'function') {
-    return 'Error';
-  }
-
-  if ( PLN <= 0 ) {
-    return '$0.00'
-  }
-
-
-  const PLNtoUSD = PLN / 3.5;
-  
+  const PLNtoUSD = PLN / USD_TO_PLN;
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
   });
 
-  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  const isPlnNotANumber = Boolean(PLN / 0) === false
+
+  if (typeof PLN === 'undefined') {
+    return NaN
+  }
+  if (typeof PLN === 'object') {
+    return 'Error';
+  }
+  if (typeof PLN === 'function') {
+    return 'Error';
+  }
+  if (isPlnNotANumber) {
+    return NaN
+  }
+  if (PLN <= 0) {
+    return '$0.00'
+  }
+
+  return formatter.format(PLNtoUSD)
+
 }
