9c3bf52f21a76eaafe9c9d5509f83062f71f4962
Slawek Derabin <slawomirderabin@gmail.com>
2023-04-25 16:16:16 +0200

    CurrencyFormTest

--- src/components/CurrencyForm/CurrencyForm.js
+++ src/components/CurrencyForm/CurrencyForm.js
@@ -3,6 +3,7 @@ import TextInput from './../TextInput/TextInput';
 import Select from './../Select/Select';
 import Button from './../Button/Button';
 import styles from './CurrencyForm.module.scss';
+import React from 'react';
 
 const CurrencyForm = ({ action }) => {
   const [amount, setAmount] = useState('');
@@ -23,18 +24,18 @@ const CurrencyForm = ({ action }) => {
     <form onSubmit={handleSubmit} className={styles.form}>
       <label>
         <span>Amount:</span>
-        <TextInput type="number" value={amount} onChange={e => setAmount(e.target.value)} />
+        <TextInput type="number" value={amount} onChange={e => setAmount(e.target.value)} data-testid="amount" />
       </label>
       <label>
         <span>From</span>
-        <Select onChange={e => setFrom(e.target.value)}>
+        <Select onChange={e => setFrom(e.target.value)} data-testid="from-select">
           <option value="PLN">PLN</option>
           <option value="USD">USD</option>
         </Select>
       </label>
       <label>
         <span>To</span>
-        <Select onChange={e => setTo(e.target.value)}>
+        <Select onChange={e => setTo(e.target.value)} data-testid="to-select">
           <option value="PLN">PLN</option>
           <option value="USD">USD</option>
         </Select>
